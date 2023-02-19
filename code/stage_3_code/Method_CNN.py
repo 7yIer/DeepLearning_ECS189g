c6c9ee8ed3ff01cc4b34b34c2bf0d6c8286a199a
Nayeel Imtiaz <naimtiaz@ucsc.edu>
2023-02-18 17:17:47 -0800

    Method_cnn

--- code/stage_3_code/Method_CNN.py
+++ code/stage_3_code/Method_CNN.py
@@ -1,4 +1,101 @@
-# Method_CNN.py
 '''
-Main file
+Concrete MethodModule class for a specific learning MethodModule
 '''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+from code.base_class.method import method
+from code.stage_1_code.Evaluate_Accuracy import Evaluate_Accuracy
+import torch
+from torch import nn
+import numpy as np
+
+
+class Method_CNN(method, nn.Module):
+    data = None
+    # it defines the max rounds to train the model
+    max_epoch = 500
+    # it defines the learning rate for gradient descent based optimizer for model learning
+    learning_rate = 1e-3
+
+    # it defines the the MLP model architecture, e.g.,
+    # how many layers, size of variables in each layer, activation function, etc.
+    # the size of the input/output portal of the model architecture should be consistent with our data input and desired output
+    def __init__(self, mName, mDescription):
+        method.__init__(self, mName, mDescription)
+        nn.Module.__init__(self)
+        # check here for nn.Linear doc: https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(32 * 8 * 8, 10)
+
+    # it defines the forward propagation function for input x
+    # this function will calculate the output layer by layer
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+
+        return x
+
+    # backward error propagation will be implemented by pytorch automatically
+    # so we don't need to define the error backpropagation function here
+
+    def train(self, X, y):
+        # check here for the torch.optim doc: https://pytorch.org/docs/stable/optim.html
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        # check here for the nn.CrossEntropyLoss doc: https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+        loss_function = nn.CrossEntropyLoss()
+        # for training accuracy investigation purpose
+        accuracy_evaluator = Evaluate_Accuracy('training evaluator', '')
+
+        # it will be an iterative gradient updating process
+        # we don't do mini-batch, we use the whole input as one batch
+        # you can try to split X and y into smaller-sized batches by yourself
+        for epoch in range(self.max_epoch): # you can do an early stop if self.max_epoch is too much...
+            # get the output, we need to covert X into torch.tensor so pytorch algorithm can operate on it
+            y_pred = self.forward(torch.FloatTensor(np.array(X)))
+            # convert y to torch.tensor as well
+            y_true = torch.LongTensor(np.array(y))
+            # calculate the training loss
+            train_loss = loss_function(y_pred, y_true)
+
+            # check here for the gradient init doc: https://pytorch.org/docs/stable/generated/torch.optim.Optimizer.zero_grad.html
+            optimizer.zero_grad()
+            # check here for the loss.backward doc: https://pytorch.org/docs/stable/generated/torch.Tensor.backward.html
+            # do the error backpropagation to calculate the gradients
+            train_loss.backward()
+            # check here for the opti.step doc: https://pytorch.org/docs/stable/optim.html
+            # update the variables according to the optimizer and the gradients calculated by the above loss.backward function
+            optimizer.step()
+
+            if epoch%100 == 0:
+                accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
+                print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
+
+    def test(self, X):
+        # do the testing, and result the result
+        y_pred = self.forward(torch.FloatTensor(np.array(X)))
+        # convert the probability distributions to the corresponding labels
+        # instances will get the labels corresponding to the largest probability
+        return y_pred.max(1)[1]
+
+    def run(self):
+        print('method running...')
+        print('--start training...')
+        self.train(self.data['train']['X'], self.data['train']['y'])
+        print('--start testing...')
+        pred_y = self.test(self.data['test']['X'])
+        return {'pred_y': pred_y, 'true_y': self.data['test']['y']}
+            
